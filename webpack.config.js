aff673c21f457a6e76d880ce78d2ee2194d0fd92
Star Bist <malimirkeccita@gmail.com>
2017-07-09 19:17:28 +0200

    Fixing webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
+    publicPath: '/',
     filename: '[name].js'
   },
   module: {
