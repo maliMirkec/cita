7e90acdb0acd7034dc15af437f1e830aac6884b5
Star Bist <malimirkeccita@gmail.com>
2017-08-16 16:11:28 +0200

    Making redesign.

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,94 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
-const DEBUG = process.env.NODE_ENV === 'development';
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const StyleExtHtmlWebpackPlugin = require("style-ext-html-webpack-plugin");
+const DEBUG = process.env.NODE_ENV === "development";
 const extractCritical = new ExtractTextPlugin({
-  filename: 'critical.css',
+  filename: "critical.css",
   disable: DEBUG
 });
 const extractStyle = new ExtractTextPlugin({
-  filename: 'style.css',
+  filename: "style.css",
   disable: DEBUG
 });
-const HtmlWebpackExcludeAssetsPlugin = require('html-webpack-exclude-assets-plugin');
+const HtmlWebpackExcludeAssetsPlugin = require("html-webpack-exclude-assets-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].js'
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].js"
   },
   module: {
-    rules: [{
-      test: /\.html$/,
-      use: [{
-        loader: 'html-loader?minimize=true&interpolate=true'
-      }],
-    }, {
-      test: /critical.scss/,
-      use: extractCritical.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader'
-        }, {
-          loader: 'postcss-loader'
-        }, {
-          loader: 'sass-loader'
-        }]
-      })
-    }, {
-      test: /style.scss/,
-      use: extractStyle.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader'
-        }, {
-          loader: 'postcss-loader'
-        }, {
-          loader: 'sass-loader'
-        }]
-      })
-    }, {
-      test: /\.svg$/,
-      use: [{
-        loader: 'svg-inline-loader?idPrefix=true'
-      }]
-    }]
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader?minimize=true&interpolate=true"
+          }
+        ]
+      },
+      {
+        test: /critical.scss/,
+        use: extractCritical.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader"
+            },
+            {
+              loader: "postcss-loader"
+            },
+            {
+              loader: "sass-loader"
+            }
+          ]
+        })
+      },
+      {
+        test: /style.scss/,
+        use: extractStyle.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader"
+            },
+            {
+              loader: "postcss-loader"
+            },
+            {
+              loader: "sass-loader"
+            }
+          ]
+        })
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-inline-loader?idPrefix=true"
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     extractCritical,
     extractStyle,
-    new StyleExtHtmlWebpackPlugin(DEBUG ? false : 'critical.css'),
+    new StyleExtHtmlWebpackPlugin(DEBUG ? false : "critical.css"),
+    new StyleExtHtmlWebpackPlugin(DEBUG ? false : "style.css"),
     new FaviconsWebpackPlugin({
-      logo: './gfx/logo/c-line.svg',
-      prefix: 'icons/',
+      logo: "./gfx/logo/CiTA-rect.svg",
+      prefix: "icons/",
       emitStats: false,
-      statsFilename: 'iconstats.json',
+      statsFilename: "iconstats.json",
       persistentCache: false,
       inject: true,
-      background: '#c1177a',
-      title: 'CiTA',
+      background: "#c1177a",
+      title: "CiTA",
       icons: {
         android: true,
         appleIcon: true,
@@ -85,24 +103,24 @@ module.exports = {
       }
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/hr.html',
-      filename: 'hr.html',
+      template: "./src/templates/hr.html",
+      filename: "hr.html",
       excludeAssets: /style.css/
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html',
-      filename: 'index.html',
+      template: "./src/templates/index.html",
+      filename: "index.html",
       excludeAssets: /style.css/
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/404.html',
-      filename: '404.html',
+      template: "./src/templates/404.html",
+      filename: "404.html",
       excludeAssets: /style.css/
     }),
     new HtmlWebpackExcludeAssetsPlugin()
   ],
   devServer: {
-    contentBase: path.join(__dirname, '/dist/'),
+    contentBase: path.join(__dirname, "/dist/"),
     port: 2309
   }
 };
